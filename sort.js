b90e0db3df59d5e158517a40802e4ec75259414c
Milton Castro <castro.milton07@gmail.com>
2021-05-26 17:44:39 -0300

    03 - The End

--- sort.js
+++ sort.js
@@ -2,4 +2,12 @@ const numbers = [3, 4, 5, 99, 8, 9, 2, 1, 12, 289, 43, 90];
 
 numbers.sort((a, b) => a - b);
 
+
 console.log(numbers);
+console.log(numbers[0]);
+
+const games = ['PES', 'Age of Empires', 'CS:GO', 'Worms'];
+
+console.log(games.sort());
+
+console.log(games.sort().reverse());
