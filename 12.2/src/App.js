dec13cf485f077e8815c0e0023b2fa1781c7a156
Milton Castro <castro.milton07@gmail.com>
2021-06-28 18:44:36 -0300

    00 - Inicializando a branch referente ao exemplo da aula 12.2

---
+++ 12.2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Olá Mundo!
+      </div>
+    );
+  }
+}
+
+export default App;
